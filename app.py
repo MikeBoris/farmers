0ef385955f9d82ae71b67656f6e3601471626211
MikeBoris <mikeboris123@gmail.com>
2017-07-02 11:58:18 -0400

    added app.py, index.html template

---
+++ app.py
@@ -0,0 +1,34 @@
+import requests
+from flask import Flask, render_template, request, url_for
+
+app = Flask(__name__)
+
+URL = "http://search.ams.usda.gov/farmersmarkets/v1/data.svc/zipSearch?zip={}"
+
+"""
+for result in results['results']:
+    print result
+
+market = "http://search.ams.usda.gov/farmersmarkets/v1/data.svc/mktDetail?id="
+
+for result in results['results']:
+    id = result['id']
+    details = requests.get(market + id).json()
+    print details['marketdetails']['GoogleLink']
+"""
+@app.route('/')
+@app.route('/farm', methods=['GET', 'POST'])
+def main():
+	if request.method == 'GET':
+		return render_template('index.html')
+	else:
+		# the request is POST
+		zip = request.form['zip_input']
+		# apply user-provided zipcode in url
+		submit_url = URL.format(zip)
+		url = requests.get(submit_url)
+		results = url.json()
+		return render_template('index.html', results=results)
+
+if __name__ == '__main__':
+	app.run(debug=True, use_reloader=True)
