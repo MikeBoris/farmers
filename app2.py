bd6d0ffc3da728d06379f40bfa746dbeaa24d356
MikeBoris <mikeboris123@gmail.com>
2017-08-17 23:29:46 -0400

    moved api and data handling to js; switched css to bulma

--- app2.py
+++ app2.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def main():
-	return render_template('home.html')
+	return render_template('dalmation.html')
 
 if __name__ == '__main__':
 	app.run(debug=True, use_reloader=True)
